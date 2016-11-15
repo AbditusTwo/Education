08cec3f6790571910ffdf24702e1defc90bf8c98
AStarodubtcev <astarodubtcev@luxoft.com>
2016-11-15 16:20:29 +0100

    Hackerrank [Java if-else] task.

---
+++ HackerRank/src/JavaIfElse.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaIfElse {
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String ans="";
+        if(n%2==1){
+            ans = "Weird";
+        }
+        else{
+            if () {
+
+            }
+        }
+        System.out.println(ans);
+    }
+}
+
+
+/*
+If  is n odd, print Weird
+If  is n even and in the inclusive range of 2 to 5, print Not Weird
+If  is n even and in the inclusive range of 6 to 20, print Weird
+If  is n even and greater than 20, print Not Weird
+ */
