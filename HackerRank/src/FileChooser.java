25f347fcb0ca7af7c59952509b77013129979d13
AStarodubtcev <astarodubtcev@luxoft.com>
2016-11-22 15:05:08 +0100

    Hackerrank [JavaLoop] task; FileChooser practice.

--- HackerRank/src/FileChooser.java
+++ HackerRank/src/FileChooser.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.*;
 
 public class FileChooser {
     public static void main (String[] args) {
+        BufferedReader br = null;
+        BufferedWriter bw = null;
         JFileChooser chooser = new JFileChooser();
         FileNameExtensionFilter filter = new FileNameExtensionFilter(
         "JPG & GIF Images", "jpg", "gif");
@@ -10,11 +13,31 @@ public class FileChooser {
 
         int returnVal = chooser.showOpenDialog(chooser);
 
-        if(returnVal == JFileChooser.APPROVE_OPTION) {
-            System.out.println("You chose to open this file: " +
-                chooser.getSelectedFile().getName());
+        try {
+
+            String sCurrentString;
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
+
+                br = new BufferedReader(new FileReader(chooser.getSelectedFile().getAbsolutePath()));
+
+                bw = new BufferedWriter(new FileWriter(chooser.getCurrentDirectory() + "myfile.txt"));
+
+                while ((sCurrentString = br.readLine()) != null) {
+                    //Do something
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) br.close();
+                if (bw != null) bw.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
-
-
     }
 }
--- HackerRank/src/JavaLoopTwo.java
+++ HackerRank/src/JavaLoopTwo.java
@@ -5,18 +5,39 @@ import java.math.*;
 import java.util.regex.*;
 
 public class JavaLoopTwo {
-    public static void main(String[] args) {
+    public static void main(String []argh){
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int i = 1;
-        if ((N>=2)&&(N<=20)) {
-            while (i <= 10) {
-                System.out.println(N + " x " + i + " = " + N*i);
-                ++i;
-            }
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
         }
-        else {
-            System.out.println("Nope");
-        }
+        in.close();
+
+        System.out.println("a");
     }
 }
+
+/*
+Input Format
+
+The first line contains an integer, q, denoting the number of queries.
+Each line i of the q subsequent lines contains three space-separated integers describing the respective , bi, and ni values for that query.
+
+Constraints
+
+Output Format
+
+For each query, print the corresponding series on a new line. Each series must be printed in order as a single line of  space-separated integers.
+
+Sample Input
+
+2
+0 2 10
+5 3 5
+Sample Output
+
+2 6 14 30 62 126 254 510 1022 2046
+8 14 26 50 98
+*/
