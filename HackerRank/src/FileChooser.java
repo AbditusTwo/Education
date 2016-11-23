e4fdcd70c0136109ba08695fcb63c12eb1210dfa
AStarodubtcev <astarodubtcev@luxoft.com>
2016-11-23 16:36:15 +0100

    FileCooser modified to use try-with-resource.

--- HackerRank/src/FileChooser.java
+++ HackerRank/src/FileChooser.java
@@ -3,9 +3,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.*;
 
 public class FileChooser {
-    public static void main (String[] args) {
-        BufferedReader br = null;
-        BufferedWriter bw = null;
+    public static void main (String[] args) throws IOException {
         JFileChooser chooser = new JFileChooser();
         FileNameExtensionFilter filter = new FileNameExtensionFilter(
         "JPG & GIF Images", "jpg", "gif");
@@ -13,30 +11,18 @@ public class FileChooser {
 
         int returnVal = chooser.showOpenDialog(chooser);
 
-        try {
+        try (BufferedReader br = new BufferedReader(new FileReader(chooser.getSelectedFile().getAbsolutePath()));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(chooser.getCurrentDirectory() + "\\myfile.txt"))
+        ) {
 
             String sCurrentString;
 
             if (returnVal == JFileChooser.APPROVE_OPTION) {
                 System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
 
-                br = new BufferedReader(new FileReader(chooser.getSelectedFile().getAbsolutePath()));
-
-                bw = new BufferedWriter(new FileWriter(chooser.getCurrentDirectory() + "myfile.txt"));
-
                 while ((sCurrentString = br.readLine()) != null) {
                     //Do something
                 }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (br != null) br.close();
-                if (bw != null) bw.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
             }
         }
     }
