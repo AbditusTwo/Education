a6dbda287f04ba47aa11e1381c17ab5d67c64433
AStarodubtcev <astarodubtcev@luxoft.com>
2016-12-16 11:04:44 +0100

    EVE scripting started.

---
+++ EVE/src/GetProcess.java
@@ -0,0 +1,5 @@
+public class GetProcess {
+    public static void main (String[] args) {
+
+    }
+}
---
+++ EVE/src/InDock.java
@@ -0,0 +1,5 @@
+public class InDock {
+    public static void main (String[] args) {
+        System.out.printf("Hello World");
+    }
+}
