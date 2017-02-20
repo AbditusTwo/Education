669a3fcb4c57f74c59c4910567c2d38c4e25e79a
AbditusTwo <abditus2.0@gmail.com>
2017-02-20 15:09:46 +0100

    Notes app

--- AngularJS/src/Notes/server.js
+++ AngularJS/src/Notes/server.js
@@ -1,8 +1,32 @@
+var express = require('express');
 var path = require('path');
+
+var session = require('express-session');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded (
+    {
+        extended: true
+    })
+);
+    app.use(bodyParser.json());
+
+app.use(
+    session(
+        {
+            secret: "angular_tutorial",
+            resave: true,
+            saveUninitialized: true
+        }
+    )
+);
+
+app.get('/notes', function (req, res) {
     var notes = [
         {text: "first note"},
         {text: "second node"},
@@ -12,6 +36,4 @@ app.use(express.static(path.join(__dirname, 'public')));
     res.send(notes);
 });
 
-
 app.listen(3000);
-
