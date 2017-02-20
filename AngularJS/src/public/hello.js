6c9ae4ea66e74e22c82f779c9b16e4e664427cca
AbditusTwo <abditus2.0@gmail.com>
2017-02-20 14:15:30 +0100

    Adding http injection

--- AngularJS/src/public/hello.js
+++ AngularJS/src/public/hello.js
@@ -1,10 +1,16 @@
 angular.module("myapp", [])
-    .controller("HelloController", function ($scope) {
-    $scope.greeting = "";
+    .controller("HelloController", function ($scope, $http) {
+        $scope.greeting = "";
 
-    $scope.update = function () {
-        if ($scope.name) {
-            $scope.greeting = "Hello " + $scope.name + "!";
+        $scope.update = function () {
+            if ($scope.name) {
+                $http.get('/greeting', {
+                    params: {
+                        name: $scope.name
+                }
+                }).then(function (res) {
+                    $scope.greeting = res.data;
+                });
             }
         }
     });
