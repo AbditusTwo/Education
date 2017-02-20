a0fbaf36173428981bfca2dba20fb900d313d0b9
AbditusTwo <abditus2.0@gmail.com>
2017-02-20 13:05:48 +0100

    Learning AngularJS + Express

--- AngularJS/src/server.js
+++ AngularJS/src/server.js
@@ -4,3 +4,9 @@ var path = require('path');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/greeting', function (req, res) {
+    res.send("Hello " + req.query.name + "! I'm Server!");
+});
+
+app.listen(3000); //We start server on port 3000
