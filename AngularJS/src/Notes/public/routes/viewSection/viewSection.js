1df9ab8509f9c23572a30e5e10a29eacbc4c15c6
AbditusTwo <abditus2.0@gmail.com>
2017-02-22 14:33:31 +0100

    Angular Task 11

--- AngularJS/src/Notes/public/routes/viewSection/viewSection.js
+++ AngularJS/src/Notes/public/routes/viewSection/viewSection.js
@@ -1,7 +1,3 @@
-/**
- * Created by BKuczynski on 2017-02-17.
- */
-
 module.controller("ViewSectionController",
     function ($scope, $http, $routeParams) {
         $scope.section = $routeParams.name;
